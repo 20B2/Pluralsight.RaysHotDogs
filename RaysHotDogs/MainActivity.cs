f731061de5bc847f8e303a14b7fd2ae1ab2b4127
Binod Marikhu <bnod_468@hotmail.com>
2017-05-01 15:52:53 +0545

    Hot Dot Details View Created

---
+++ RaysHotDogs/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace RaysHotDogs
+{
+    [Activity(Label = "RaysHotDogs", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            int count = 1; 
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+
+            button.Click += delegate
+            {
+                button.Text = string.Format("{0} clicks!", count++);
+            };
+        }
+    }
+}
+
